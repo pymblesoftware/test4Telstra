933c57a2532259c5cf877204b66bd9e47e93f5a0
Regan <regan@pymblesoftware.com>
2020-03-29 16:30:49 +1000

    Initial Commit

---
+++ TestForTelstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForTelstra
+//
+//  Created by Regan Russell on 29/3/20.
+//  Copyright © 2020 Regan Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
